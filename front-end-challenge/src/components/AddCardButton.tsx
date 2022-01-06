08326451e008ac4f523c87dd83e616aa4d53d80a
giovanni-maldonado <giovannitadeu08@gmail.com>
2022-01-05 22:30:56 -0300

    feat: componentização do botão de adiconar ao carrinho salvando no local storage

---
+++ front-end-challenge/src/components/AddCardButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Iitems } from '../interfaces/interfaces'
+import { productSlice } from '../redux/slices/productSlice'
+
+interface Props {
+  id: number,
+}
+
+const AddCardButton: React.FC<Props> = (Props) => {
+  const { products } = useSelector(productSlice)
+
+  const addToCart = (id: number) => {
+    const product = products.find((item: Iitems) => item.id === id)
+    localStorage.setItem('productInCart', JSON.stringify(product))
+  }
+
+  return (
+    <div>
+      <button
+        className="add-to-cart"
+        onClick={() => addToCart(Props.id)}
+      >
+        ADICIONAR
+      </button>
+    </div>
+  )
+}
+
+export default AddCardButton
