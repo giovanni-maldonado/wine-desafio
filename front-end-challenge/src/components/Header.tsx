352a4c8d62ff857f78082d7b0e955cb998a95141
giovanni-maldonado <giovannitadeu08@gmail.com>
2022-01-08 16:32:25 -0300

    feat: colocando um onClick no botão do Header

--- front-end-challenge/src/components/Header.tsx
+++ front-end-challenge/src/components/Header.tsx
@@ -1,7 +1,9 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 import { HeaderStyle } from '../styles/components/Header'
 
-const Header = () => {
+const Header: React.FC = () => {
+  const router = useRouter()
   return (
     <HeaderStyle>
       <img src="https://d2duuy9yo5pldo.cloudfront.net/dashboard-resources/wine/e90faa36-9670-4bd4-a063-8818d3f9d20e.png" alt="logo da wine"/>
@@ -16,7 +18,10 @@ const Header = () => {
       <button className="header-button">
         <img className="header-icon" src="https://cdn-icons-png.flaticon.com/512/848/848043.png" alt="avatar anonimo"/>
       </button>
-      <button className="header-button">
+      <button
+        onClick={ () => router.push('/cart') }
+        className="cart-icon-button"
+      >
         <img className="header-icon" src="https://cdn-icons-png.flaticon.com/512/743/743131.png" alt="sacola de compras"/>
       </button>
     </HeaderStyle>
