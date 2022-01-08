cff5a1e7104a7d464759602657de2fe3843fca55
giovanni-maldonado <giovannitadeu08@gmail.com>
2022-01-07 22:42:53 -0300

    fix: alterando o local do Provider do index.tsx para _app.tsx

--- front-end-challenge/src/pages/_app.tsx
+++ front-end-challenge/src/pages/_app.tsx
@@ -3,13 +3,17 @@ import type { AppProps } from 'next/app'
 import GlobalStyle from '../styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from '../styles/theme'
+import { Provider } from 'react-redux'
+import store from '../redux/store'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </Provider>
   )
 }
 
--- front-end-challenge/src/pages/index.tsx
+++ front-end-challenge/src/pages/index.tsx
@@ -2,21 +2,19 @@ import React from 'react'
 import Head from 'next/head'
 import { Container } from '../styles/pages/Home'
 import ProductList from '../components/ProductList'
-import store from '../redux/store/index'
-import { Provider } from 'react-redux'
 import Header from '../components/Header'
+import FilterCatalog from '../components/FilterCatalog'
 
 const Home: React.FC = () => {
   return (
-    <Provider store={store}>
-      <Container>
-        <Head>
-          <title>Next App</title>
-        </Head>
-        <Header/>
-        <ProductList />
-      </Container>
-    </Provider>
+    <Container>
+      <Head>
+        <title>Wine App</title>
+      </Head>
+      <Header/>
+      <ProductList />
+      <FilterCatalog />
+    </Container>
   )
 }
 
